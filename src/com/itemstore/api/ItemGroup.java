9bb08e8be20b49c926261d907692162b1843e47d
miguelkrantz <miguel.krantz@gmail.com>
2017-08-07 14:15:41 +0200

    test

--- src/com/itemstore/api/ItemGroup.java
+++ src/com/itemstore/api/ItemGroup.java
@@ -21,32 +21,28 @@ public class ItemGroup {
     private static final Logger LOG = Logger.getLogger(ItemGroup.class.getName());
 
     @GET
-    @Path("/search")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response searchItemGroups(@Context HttpServletRequest httpServletRequest,
-                                     @QueryParam("includeTagTreeFilter") String includeTagTreeFilter,
-                                     @QueryParam("excludeTagTreeFilter") String excludeTagTreeFilter,
-                                     @QueryParam("excludeItemGroupIds") List<Integer> excludeItemGroupIds) {
+    public Response getAllItemGroups(@Context HttpServletRequest httpServletRequest) {
 
-        SearchItemGroupQuery searchQuery;
+        List<com.itemstore.engine.model.ItemGroup> itemGroups = ItemEngine.getInstance().getAllItemGroupsSortedByDate();
 
-        //includeTagTreeFilter
-        //excludeTagTreeFilter
-        //excludeItemGroupIds
-
-        LOG.info("includeTagTreeFilter=" + includeTagTreeFilter + ",excludeTagTreeFilter="
-                + excludeTagTreeFilter + ",excludeItemGroupIds=" + excludeItemGroupIds);
-
-        try {
-            searchQuery = SearchItemGroupQuery.create(includeTagTreeFilter, excludeTagTreeFilter, excludeItemGroupIds);
-        } catch (SearchItemGroupQuery.InvalidRequestException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
-        }
-
-        List<com.itemstore.engine.model.ItemGroup> itemGroups = ItemEngine.getInstance().search(searchQuery);
-
+        LOG.info("itemGroups = " + itemGroups.size());
         ItemGroupResponse response = ItemGroupResponse.create(itemGroups);
 
         return Response.status(Response.Status.OK).entity(response).build();
     }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getItemGroupsFromDate(@Context HttpServletRequest httpServletRequest) {
+
+        List<com.itemstore.engine.model.ItemGroup> itemGroups = ItemEngine.getInstance().getAllItemGroupsSortedByDate(); //FIXME
+
+        LOG.info("itemGroups = " + itemGroups.size());
+        ItemGroupResponse response = ItemGroupResponse.create(itemGroups);
+
+        return Response.status(Response.Status.OK).entity(response).build();
+    }
+
 }
