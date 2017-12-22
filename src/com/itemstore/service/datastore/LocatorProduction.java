ab4b2c739cc6fccadb3425211f4bc58bea4367c0
Miguel Krantz <miguel.krantz@telenor.se>
2017-12-22 23:43:37 +0100

    test build

--- src/com/itemstore/service/datastore/LocatorProduction.java
+++ src/com/itemstore/service/datastore/LocatorProduction.java
@@ -4,40 +4,25 @@ import java.io.File;
 
 public class LocatorProduction implements HomeDirectoryLocator {
 
-    private static final String OPENSHIFT_DATA_DIR = "OPENSHIFT_DATA_DIR";
-
     private static final String ITEM_STORE_DATA_HOME = "ITEM_STORE_DATA_HOME";//Must exist before starting app! //FIXME
 
     @Override
     public File getHomeDirectory() {
-        File podDataHomeDir = new File("/home/krantmig/tools/temp" + File.separator + ITEM_STORE_DATA_HOME);
-        if (isReadAndWriteDirectory(podDataHomeDir)) {
-            return podDataHomeDir;
-        }
 
-        podDataHomeDir = new File("/Users/miguelkrantz/Documents/temp/" + File.separator + ITEM_STORE_DATA_HOME);
-        if (isReadAndWriteDirectory(podDataHomeDir)) {
-            return podDataHomeDir;
-        }
+        File   homeDirectory = new File("/tmp/", ITEM_STORE_DATA_HOME);
 
-        String openShiftDataDir = System.getenv(OPENSHIFT_DATA_DIR);
-
-        if (openShiftDataDir != null) {
-            podDataHomeDir = new File(openShiftDataDir, ITEM_STORE_DATA_HOME);
-        }
-
-        if (!podDataHomeDir.exists()) {
-            if (!podDataHomeDir.mkdirs()) {
-                throw new IllegalStateException("Unable to create dirs " + podDataHomeDir.getAbsolutePath());
+        if (!homeDirectory.exists()) {
+            if (!homeDirectory.mkdirs()) {
+                throw new IllegalStateException("Unable to create dirs " + homeDirectory.getAbsolutePath());
             }
         }
 
-        if (isReadAndWriteDirectory(podDataHomeDir)) {
-            return podDataHomeDir;
+        if (isReadAndWriteDirectory(homeDirectory)) {
+            return homeDirectory;
 
         }
 
-        throw new IllegalStateException("Unable to locate home directory");
+        throw new IllegalStateException("Unable to locate home directory " + homeDirectory.getAbsolutePath());
 
     }
 
